ac891225f404c4571ba07fafd68e7e66bba8ea62
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-01-05 21:21:36 +0545

    attempt ordering in newest first

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -4,6 +4,8 @@ export const instance = axios.create({
     baseURL:process.env.NEXT_PUBLIC_BASEURL || "http://202.79.51.253:7070/api/"
 }) 
 
+
+
 instance.interceptors.request.use((config)=>{
     if (config.data instanceof FormData){
         delete config.headers["Content-Type"]
