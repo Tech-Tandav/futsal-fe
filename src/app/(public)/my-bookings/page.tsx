2a2c3c0ad9132d411fa78383336279628fafaed5
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-01-23 22:37:06 +0545

    done

--- src/app/(public)/my-bookings/page.tsx
+++ src/app/(public)/my-bookings/page.tsx
@@ -89,7 +89,7 @@ export default function MyBookingsPage() {
                     <td className="px-3 py-2 font-medium">{booking.date}</td>
                     <td className="px-3 py-2 font-medium"> {booking.startTime}</td>
                     <td className="px-3 py-2 font-medium"> {getStatusBadge(booking.status)}</td>
-                    <td className="px-3 py-2 font-medium"> {date}</td>
+                    <td className="px-3 py-2 font-medium"> {new Date(date).toLocaleString()}</td>
                   </tr>
                 )})}
               </tbody>
