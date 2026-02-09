df0d525fd45323221b2fb449a265109799f7e5c6
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-02-09 23:52:36 +0545

    fix

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,10 +1,6 @@
 import * as React from "react"
 
-<<<<<<< HEAD
-import { cn } from "@/src/lib/utils"
-=======
 import { cn } from "@/lib/utils"
->>>>>>> 6cf2cbed1759f2c4bd1a9ad937f7e51cdd3f183f
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
